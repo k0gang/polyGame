179561f441a8834acf04cbef476310f4f12d2c5f
k0gang <k0gang@naver.com>
2024-04-01 14:36:45 +0900

    Add : Main

--- polyGame/src/polyGame/Main.java
+++ polyGame/src/polyGame/Main.java
@@ -1,5 +1,15 @@
 package polyGame;
 
 public class Main {
-
+	public static void main(String[] args) {
+		GameManager gameManager = new GameManager();
+		boolean run = true;
+		while(true) {
+			run = gameManager.changeStage();
+			if(run == false) {
+				break;
+			}
+		}
+		System.out.println("GAME OVER");
+	}
 }
