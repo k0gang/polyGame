2a4c27f0738532cbe0c11457349470d0b0459cad
k0gang <k0gang@naver.com>
2024-04-02 12:18:18 +0900

    Add : StageBattle init, update

--- polyGame/src/polyGame/StageBattle.java
+++ polyGame/src/polyGame/StageBattle.java
@@ -1,5 +1,35 @@
 package polyGame;
 
-public class StageBattle extends Stage {
+import java.util.Random;
+import java.util.Vector;
 
+public class StageBattle extends Stage {
+	UnitManager unitManager = new UnitManager();
+	Vector<Player> playerList = null;
+	Vector<Unit> monList = null;
+	Random ran = new Random();
+	int monDead = 0;
+	int playerDead = 0;
+	
+	@Override
+	public void init() {
+		unitManager.mon_list.clear();
+		unitManager.monster_rand_set(4);
+		playerList = null;
+		playerList = unitManager.player_list;
+		monList = null;
+		monList = unitManager.mon_list;
+		monDead = monList.size();
+		playerDead = playerList.size();
+	}
+	
+	
+	
+	
+	@Override
+	public boolean update() {
+		
+		return false;
+	}
+	
 }
