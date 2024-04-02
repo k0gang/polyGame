f382fe0a2ee65e9cca51f0742e641e9598209531
k0gang <k0gang@naver.com>
2024-04-02 10:39:57 +0900

    Add : StageLobby

--- polyGame/src/polyGame/StageLobby.java
+++ polyGame/src/polyGame/StageLobby.java
@@ -1,5 +1,26 @@
 package polyGame;
 
+import javax.naming.ldap.InitialLdapContext;
+
 public class StageLobby extends Stage {
 
+	@Override
+	public boolean update() {
+		System.out.println("=====[LOBBY]=====");
+		System.out.println("[1.전투] [2.종료]");
+		int sel = GameManager.sc.nextInt();
+		if(sel == 1)
+			GameManager.nextStage = "BATTLE";
+		else if(sel == 2)
+			GameManager.nextStage = "";
+		else
+			GameManager.nextStage = "";
+		
+		return false;
+	}
+	
+	@Override
+	public void init() {
+		
+	}
 }
