9787bc87e9a9d8bd29f85785b3834a3ef5c8fdca
k0gang <k0gang@naver.com>
2024-04-02 14:33:19 +0900

    Add : GameManager changeStage

--- polyGame/src/polyGame/GameManager.java
+++ polyGame/src/polyGame/GameManager.java
@@ -14,7 +14,35 @@ public class GameManager {
 
 	GameManager(){
 		
+		stageList.put("TITLE", new StageTitle());
+		stageList.put("BATTLE", new StageBattle());
+		stageList.put("LOBBY", new StageLobby());
 		
+		nextStage = "TITLE";
+	}
+	
+	public boolean changeStage() {
+		System.out.println("curStage : " + curStage);
+		System.out.println("nextStage : " + nextStage);
+		
+		if(curStage.equals(nextStage))
+			return true;
+		
+		curStage = nextStage;
+		Stage stage = stageList.get(curStage);
+		stage.init();
+		
+		boolean run = true;
+		while(true) {
+			run = stage.update();
+			if(run == false)
+				break;
+		}
+		
+		if(nextStage.equals(""))
+			return false;
+		else
+			return true;
 		
 	}
 	
