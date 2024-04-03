527bac2298e4f16319acc27429a0d33e06ad39d1
k0gang <k0gang@naver.com>
2024-04-03 14:23:31 +0900

    Add : Magician, Healer skill

--- polyGame/src/polyGame/Healer.java
+++ polyGame/src/polyGame/Healer.java
@@ -6,10 +6,20 @@ public class Healer extends Unit implements skillUsable {
 
 	Healer() {
 		name = "성직자";
+		maxhp = 500;
+		curhp = 500;
+		power = 30;
 	}
 
 	public void skill(Vector<Unit> unitList, int index) {
-		// 스킬
+		System.out.println("성직자의 치유의 물결!");
+		System.out.println("전체 아군 체력을 [" + power +"X3]만큼 회복");
+		
+		for(int i=0; i<unitList.size(); i++) {
+			unitList.get(i).curhp += power*3;
+			if(unitList.get(i).curhp > maxhp)
+				unitList.get(i).curhp = maxhp;
+		}
 	}
 
 }
--- polyGame/src/polyGame/Magician.java
+++ polyGame/src/polyGame/Magician.java
@@ -3,13 +3,28 @@ package polyGame;
 import java.util.Vector;
 
 public class Magician extends Unit implements skillUsable {
-
+	
+	public int maxmp;
+	public int curmp;
+	
 	Magician() {
 		name = "법사";
+		maxhp = 800;
+		curhp = 800;
+		power = 60;
 	}
 
 	public void skill(Vector<Unit> unitList, int index) {
-		// 스킬
+		System.out.println("법사의 메테오!");
+		System.out.println("모든 적에게 ["+power+"X2] 의 데미지");
+		
+		for(Unit unit : unitList) {
+			unit.curhp -= power*2;
+			if(unit.curhp <= 0) {
+				System.out.println("[" +unit.name+"]가 쓰러졌다!");
+				unit.curhp = 0;
+			}
+		}
 	}
 
 }
