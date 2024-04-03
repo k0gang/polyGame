aa3fc3e43ead7ffe95bcca6845587f9bc16ce5a1
k0gang <k0gang@naver.com>
2024-04-03 10:19:58 +0900

    Update : skill

--- polyGame/src/polyGame/UnitWolf.java
+++ polyGame/src/polyGame/UnitWolf.java
@@ -9,10 +9,16 @@ public class UnitWolf extends Unit implements skillUsable {
 	}
 
 	public void skill(Vector<Player> playerList, int index) {
+		System.out.println("늑대 전용 스킬 발동!");
 		System.out.println("적 전체에게 공격력의 절반의 물리데미지");
 
 		for (int i = 0; i < playerList.size(); i++) {
 			playerList.get(i).curhp -= power / 2;
+			System.out.println("[" + playerList.get(i).name + "] 에게 [" + power + "/2]의 데미지");
+			if (playerList.get(i).curhp <= 0) {
+				System.out.println("[" + playerList.get(i).name + "] 가 쓰러졌다!");
+				playerList.get(i).curhp = 0;
+			}
 		}
 
 	}
